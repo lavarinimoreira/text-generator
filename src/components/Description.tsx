c64dbc455baa08fb8ad06ba42ea30350592c77d5
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-01-29 20:22:40 -0300

    updating Description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -4,7 +4,7 @@ import './Description.css'
 const Description = () => {
   return (
     <article className='description'>
-        The text is sourcered from <a href='https://en.wikipedia.org/wiki/Synthwave' target="_blank" rel="noreferrer">Wikipedia</a> .
+        The text is sourced from <a href='https://en.wikipedia.org/wiki/Synthwave' target="_blank" rel="noreferrer">Wikipedia</a> .
     </article>
   )
 }
